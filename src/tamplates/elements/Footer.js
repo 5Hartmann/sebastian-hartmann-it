47a95fa7db4e81a1e31e2e542a953cb2a6644b67
Sebsatian Hartmann <sebastianhartmann1996@live.de>
2024-01-05 15:53:42 +0100

    Add some Links CIaD

--- src/tamplates/elements/Footer.js
+++ src/tamplates/elements/Footer.js
@@ -1,8 +1,8 @@
 function Footer() {
     return ( 
         <div className="footerBox">
-            <div className="btn"><a href="/">LikedIn</a></div>
-            <div className="btn"><a href="/">gitHub</a></div>
+            <div className="btn"><a href="https://www.linkedin.com/in/sebastian-hartmann-a128201a9/">LikedIn</a></div>
+            <div className="btn"><a href="https://github.com/5Hartmann">gitHub</a></div>
             <div className="btn"><a href="/">GetInIT</a></div>
         </div>
      );
