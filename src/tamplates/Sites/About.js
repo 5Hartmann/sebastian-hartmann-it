dcf88248c3a6f68fe6352ee505772288c0780a0b
Sebastian Hartmann <sebastianhartmann1996@live.de>
2024-01-09 11:01:36 +0100

    Add first about content

--- src/tamplates/Sites/About.js
+++ src/tamplates/Sites/About.js
@@ -1,5 +1,54 @@
+import HeroImg from "../assets/Bewerbung.jpg";
 function About() {
-    return <h1>About</h1>;
+    return (
+        <div className="MainContainer" >
+            <div className="contentBlock">
+                <img id="HeroImg" src={HeroImg} alt="its Me!" />
+                <div className="textBlock About">
+                    <div className="txt">
+                        <h3 className="txtTop">
+                            Mein Name ist Sebastian Hartmann, das weist du ja schon<br /><br />
+                            Ich bin in Rüdersdorf bei Berlin geboren und seid dem auch in der Region geblieben.<br/>
+                            Heute wohne ich im beschaulichen schöneiche bei Berlin!<br/>
+                            Meine Kindheit verbrachte ich in Berlin Friedrichshagen und bin dort auch bis zur 10. Klasse zur schule gegangen.<br/>
+                            Ich hatte seid Kindesbeinen an interesse an Technik und Fahrzeugen daher entschied ich mich für den beruf des KFZ-Mechatronikers.
+                            Ich schloss also 2017 meine Ausbildung ab und arbeitete anschließend als Geselle.<br/>
+                            <br/>
+                            Naja zumindest für 3 Monate...<br/>
+                            Denn am 13.6.2017 ereignete sich für mich ein richtungsweisendes Ereignis in meinem Leben.<br/>
+                            Ich hatte einen Spontan-pneumothorx im linken Lungenflügel.<br/> 
+                            Ich landete also von Arbeit aus erst beim D-Arzt und dann im Krankenhaus.<br/>
+                            Die behandlung verlief leider nicht ganz nach Plan und so wurde die Lunge Operativ geflickt.<br/>
+                            Allerdings wurde mir dort auch schon gesagt das ich mich nach einem neuen Beruf, abseits des Handwerks, umsehen soll.<br/>
+                            <br/>
+                            Da brach für mich also erstmal meine kleine welt zusammen.<br/>
+                            Aber wie ich so bin aufgeben ist keine option also fing ich an zu überlegen...<br/>
+                            Ich hinter einem Schreibtisch eine Sache, die weder ich noch meine Eltern sich damals vorstellen konnten!<br/>
+                            Ich passte weder in eine Bank noch hinter einen Verkaufstisch also was tun...<br/>
+                            Zum glück war da meine Schwester! Diese studierte damals Informatik und hatte mir gelegentlich mal hier und da gezeigt wie man mit Java umging.
+                            Ich hatte aus interesse auch schonmal mit Unity gearbeitet und dachte mir dann, also wenn schon was hinterm schreibtisch dan was wo ich was erschaffen kann!<br/>
+                            <br/>
+                            Nun hatte ich aber kein Abitur daher kam ein studium nicht in frage... <br/>
+                            Weder als Ingenieur noch als Informatiker.<br/>
+                            Quereinstieg ohne erfahrung und können nicht möglich.<br/>
+                            doch da kam die nette frau vom arbeitsamt und fragte ob nicht der Fachinformatikerwas wäre?<br/>
+                            Also angesehen zugestimmt und die umschulung bekommen und absolviert!<br/>
+                            nun sitze ich hier, hab zwar noch nicht die riesen berufserfahrung bin aber mit Webentwicklung glücklich!<br/>
+                            grade das Frontend macht mir spaß und ich kann etwas nützliches erschaffen.<br/>
+                            ich hätte zwar nie gedacht das ich das mal sage, aberich bin tatsächlich hinter einem schreibtisch glücklich geworden!<br/>
+                            <br/>
+                            aber bevor ich hier nen Roman über mich schreibe belasse ich es dabei.<br/>
+                            Wer mehr Wissen möchte muss mich zumindest mal zu nem Kaffee einladen!
+                        </h3>
+                    </div>
+                </div>
+
+                
+
+            </div>
+        </div>
+
+    )
 }
 
 export default About;
