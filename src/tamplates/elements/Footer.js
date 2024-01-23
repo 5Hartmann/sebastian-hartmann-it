769fb06ef54575e97d9a74558518846967f149ed
Sebastian Hartmann <sebastianhartmann1996@live.de>
2024-01-23 11:36:54 +0100

    FIX getinit link

--- src/tamplates/elements/Footer.js
+++ src/tamplates/elements/Footer.js
@@ -3,7 +3,7 @@ function Footer() {
         <div className="footerBox">
             <div className="btn"><a href="https://www.linkedin.com/in/sebastian-hartmann-a128201a9/">LikedIn</a></div>
             <div className="btn"><a href="https://github.com/5Hartmann">gitHub</a></div>
-            <div className="btn"><a href="/">GetInIT</a></div>
+            <div className="btn"><a href="https://www.get-in-it.de/profil/thidUzdP96xLwJ8kftS6JNMkgcgmFZaO">GetInIT</a></div>
         </div>
      );
 }
